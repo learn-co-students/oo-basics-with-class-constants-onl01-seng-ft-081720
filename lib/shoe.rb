384e9e09c0b0a49a7e437be8ca78bed3bbd860c5
Tabatha Slatton <valbatha@gmail.com>
2020-08-21 11:26:07 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
+#  learn spec/01_book_spec.rb
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << @brand if !BRANDS.include?(brand)
   end
 
   def cobble
