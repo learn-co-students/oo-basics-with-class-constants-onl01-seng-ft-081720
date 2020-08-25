a3f6c1ce9e539ad3e71369bf95c5a9415f37ec5b
Newland-A <ajn252@gmail.com>
2020-08-25 11:21:40 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,22 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+#creates an array for the brands method
+  BRANDS = []
 
   def initialize(brand)
+    #keeps track of all brands
     @brand = brand
+    #checks to see in includes certain brands
+    if !(BRANDS.include?(@brand))
+      #pushes each brand into the BRANDS array
+      BRANDS << @brand
+    end
   end
 
   def cobble
+    #tests to see if the condition is new
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
